71553af60a1fd48029b59b84c6e3046aa9a6e009
sofamaniac <antoine.grimod@gmail.com>
2021-02-04 23:10:26 +0100

    minimal working exemple

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,9 +1,20 @@
+import scalafx.animation.AnimationTimer
 import scalafx.Includes._
 import scalafx.application.JFXApp
+import scalafx.stage.Stage
 import scalafx.scene.Scene
+import scalafx.scene.Group
+import scalafx.scene.layout.Pane
+import scalafx.scene.canvas.Canvas
+import scalafx.scene.canvas.GraphicsContext
+import scalafx.scene.paint.Color
+import scalafx.scene.text.Font
+import scalafx.scene.text.FontWeight
+import scalafx.scene.text.FontPosture;
 import scalafx.scene.paint.Color._
-import scalafx.scene.shape.Rectangle
-
+import scalafx.scene.image.Image
+import scalafx.scene.image._
+/*
 object HelloStageDemo extends JFXApp {
   stage = new JFXApp.PrimaryStage {
     title.value = "Hello Stage"
@@ -20,4 +31,57 @@ object HelloStageDemo extends JFXApp {
       }
     }
   }
+}
+*/
+
+/*
+object ImageTest extends JFXApp {
+
+  val ressource_folder = "file:src/main/ressources/"
+  stage = new JFXApp.PrimaryStage{}
+  stage.setTitle("Test Image")
+  stage.setHeight(650)
+  stage.setWidth(500)
+
+  val im = new ImageView(ressource_folder + "dome.png")
+
+  val scene = new Scene{
+    content = im
+  }
+  scene.fill = Black
+  stage.setScene(scene)
+  stage.show()
+}
+*/
+
+object ExtendedTest extends JFXApp {
+  val canvasH = 600
+  val canvasW = 800
+
+  val gameCanvas = new Canvas(canvasW, canvasH)
+
+  val ressource_folder = "file:src/main/ressources/"
+  val image = new ImageView(ressource_folder + "dome.png")
+  val gc = gameCanvas.graphicsContext2D
+
+  stage = new JFXApp.PrimaryStage
+  stage.height = canvasH
+  stage.width = canvasW 
+  val scene = new Scene
+  scene.fill = Black
+  scene.root = new Pane {children = List(gameCanvas)}
+  scene.content = image
+
+  stage.setScene(scene)
+
+  val animateTimer = AnimationTimer { t=>
+
+    image.setRotate(image.getRotate() + 2)
+  }
+  val animateTimer2 = AnimationTimer { t=>
+
+    image.setRotate(image.getRotate() -3)
+  }
+  animateTimer.start()
+  animateTimer2.start()
 }
