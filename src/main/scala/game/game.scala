788ce1dbcd58879624ca44fdac392a57a8d4bf40
Machiavelli <b.lhopit@gmail.com>
2021-02-19 16:36:10 +0100

    created Board object

---
+++ src/main/scala/game/game.scala
@@ -0,0 +1,12 @@
+class Board(val player:Player, dest:GraphicsContext)
+{
+    // map
+    // tableau d'ennemis
+    // chests, items...
+    
+    def updateBoard()
+    {
+        // actions des entités en jeu (attaques...)
+    }
+}
+
