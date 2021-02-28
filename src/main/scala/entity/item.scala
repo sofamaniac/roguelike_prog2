17748d7cdf4c494684bb3c53c148aeefb7870cb9
sofamaniac <antoine.grimod@gmail.com>
2021-02-28 21:11:53 +0100

    fixed merge problems

--- src/main/scala/entity/item.scala
+++ src/main/scala/entity/item.scala
@@ -26,59 +26,54 @@ abstract class Weapon(name:String, price:Int, rarity:Int) extends Item
   {
     1 + scala.util.Random.nextInt(max)
   }
+  def _attack(dest:Point, attacker:SentientEntity, bonus:Int) =
+  {
+    Map.fromPoint(dest).entity match
+    {
+        case None => ()
+        // TODO: if we attack empty tile
+        case Some(e) =>
+            if (roll() >= e.armorClass && !e.dodge())
+            {
+                var dmg = 0
+                var i = 0
+                for(i<-1 to numberRoll)
+                {
+                    dmg += roll(damageRoll)
+                }
+                e.damage(dmg + bonus, attacker)
+                MessageHandler.addInfo("Hit %s and dealt %d damage.".format(e.name, dmg))
+            }
+            else
+            {
+              MessageHandler.addInfo("Missed.")
+            }
+        }
+    }
 }
 
 class MeleeWeapon(val name:String, val price:Int, val rarity:Int, val innerRange:Int, val outerRange:Int, val numberRoll:Int, val damageRoll:Int) extends Weapon(name, price, rarity)
 {
   def attack(dest:Point, attacker:SentientEntity) =
   {
-    Map.fromPoint(dest).entity match
-    {
-        Map.fromPoint(dest).entity match
-        {
-            case None => ()
-            // TODO: if we attack empty tile
-            case Some(e) =>
-                if (roll() >= e.armorClass && !e.dodge())
-                {
-                    var damage = 0
-                    var i = 0
-                    for(i<-1 to numberRoll)
-                    {
-                        damage += roll(damageRoll)
-                    }
-                    e.curHP -= damage+(attacker.str/10)
-                }
-        }
-    }
+    _attack(dest, attacker, (attacker.baseStr + attacker.modifStr)/10)
+  }
 }
 
 class RangedWeapon(val name:String, val price:Int, val rarity:Int, val innerRange:Int, val outerRange:Int, val numberRoll:Int, val damageRoll:Int) extends Weapon(name, price, rarity)
 {
     def attack(dest:Point, attacker:SentientEntity) = 
-     {
-        Map.fromPoint(dest).entity match
-        {
-            case None => ()
-            // TODO: if we attack empty tile
-            case Some(e) =>
-                if (roll() >= e.armorClass && !e.dodge())
-                {
-                    var damage = 0
-                    var i = 0
-                    for(i<-1 to numberRoll)
-                    {
-                        damage += roll(damageRoll)
-                    }
-                    e.curHP -= damage+(attacker.dex/10)
-                }
-        }
+    {
+        _attack(dest, attacker, (attacker.baseDex + attacker.modifDex)/10)
     }
 }
 
 class CasterWeapon(val name:String, val price:Int, val rarity:Int, val innerRange:Int, val outerRange:Int, val numberRoll:Int, val damageRoll:Int) extends Weapon(name, price, rarity)
 {
 
-  def attack(dest:Point, attacker:SentientEntity) = {}
+  def attack(dest:Point, attacker:SentientEntity) = 
+  {
+    //attack each tile in the AoE separately ?
+  }
 }
 
